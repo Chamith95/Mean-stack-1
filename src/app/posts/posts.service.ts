1e52c8aec583a7321c9d1249fecc3ac76867f65f
Chamith95 <chamithjay@gmail.com>
2019-03-06 00:28:05 +0530

    backend node server  functions done

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -1,5 +1,6 @@
 import{ Post } from './post.model';
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 import {Subject}from 'rxjs';
 
 @Injectable({providedIn:'root'})
@@ -7,8 +8,15 @@ import {Subject}from 'rxjs';
 export class PostsService{
     private posts:Post[]=[];
     private postUpdated=new Subject<Post[]>();
+
+    constructor(private http:HttpClient){}
     getPosts(){
-        return [...this.posts];
+       this.http.get<{message:string,posts:Post[]}>('http://localhost:3000/api/posts')
+       .subscribe((postData)=>{
+          this.posts= postData.posts;
+          this.postUpdated.next([...this.posts])
+
+       });
     }
 
     getPostUpdateListner(){
@@ -17,7 +25,12 @@ export class PostsService{
 
     addPost(post1:Post){
         const post:Post=post1;
-        this.posts.push(post);
-        this.postUpdated.next([...this.posts] );
+        this.http.post<{message:string}>('http://localhost:3000/api/posts',post)
+            .subscribe((responseData)=>{
+                console.log(responseData.message);
+                this.posts.push(post);
+                this.postUpdated.next([...this.posts] );
+            });
+
     }
 }
